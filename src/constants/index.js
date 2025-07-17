e0d686c14e5d956c15357fcacb866704b7d9d9bc
yura6669 <yurii.shyian.01@gmail.com>
2025-07-17 20:56:35 +0300

    Implement swagger docs

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,3 +25,4 @@ export const CLOUDINARY = {
 
 export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
+import { swaggerDocs } from './middlewares/swaggerDocs.js'; 
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -24,6 +25,8 @@ export const setupServer = () => {
       },
     }),
     );
+  
+  app.use('/api-docs', swaggerDocs());
   
   app.use(authRouter);
   
